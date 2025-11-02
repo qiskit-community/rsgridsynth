7fbd20e01f007df4048c6ed48c26fcb63381c08a
John Lapeyre <1969884+jlapeyre@users.noreply.github.com>
2025-11-02 15:05:20 -0500

    Add more integration tests
    This PR	reveals	bugs. I	have added tests to the	integration test suite.
    Currently, I have two tests flagged with #[ignore] so that they	will not run by
    default.
    
    The bugs seem to be related to some kind of mutable state. We expect that the
    result of creating a configuration and doing a decomposition does not depend
    on previous calls. But in fact it does.
    
    As it is, the test suite passes every time I run it.
    
    In fact, the following three invocations run without error for me.
    
    > cargo test
    
    > cargo test --test '*'
    
    > cargo test --test '*' -- --ignored
    
    But this invocation will fail in various ways. Either by throwing an
    error, or by running for a couple of minutes; until I terminate the test
    
    > cargo test --test '*' -- --include-ignored
    
    There are several other interesting combinations that you can find.
    For example simple_test2 test three different seeds that all give the same
    decomposition. But if you run only the middle pair (comment out the first and third),
    then you find a different decomposition and a test failure

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,17 +1,58 @@
 use rsgridsynth::config::config_from_theta_epsilon;
 use rsgridsynth::gridsynth::gridsynth_gates;
-//use rsgridsynth::common::{PI, get_prec_bits};
 
 #[test]
+#[ignore]
 fn simple_test() {
-    // let theta = PI.clone() / to_fbig(16.0);
-    // let epsilon = to_fbig(1e-10);
     let pi = std::f64::consts::PI;
-    let theta = pi / 8.0;
+    let theta = pi / 8.0; // ≈ 0.39269908169872414
     let epsilon = 1e-10;
     let seed = 1234;
     let mut gridsynth_config = config_from_theta_epsilon(theta, epsilon, seed);
     let gates = gridsynth_gates(&mut gridsynth_config);
     let expected_gates = "HTHTSHTSHTHTSHTHTSHTHTHTSHTSHTHTHTHTHTHTSHTSHTHTSHTSHTSHTSHTHTSHTSHTSHTHTHTHTHTHTSHTSHTHTSHTSHTSHTHTHTSHTSHTSHTSHTSHTSHTSHTHTHTHTHTSHTSHTSHTSHTSHTSHTHTHTHTHTSHTHTSHTHTHTSHTSHTSHTHTSHTSHTHTSHTHTSHTSHTHTSHTHTHTSHTSHTSHTSHTHTHTHTSHTHTHTSHTHTSHTHTHTSHTHTSHTHTSHTXSSWWW";
     assert_eq!(gates, expected_gates);
+}
+
+
+#[test]
+#[ignore]
+fn simple_test2() {
+    let pi = std::f64::consts::PI;
+    let theta = pi / 8.0; // ≈ 0.39269908169872414
+    let epsilon = 1e-10;
+
+    let gates1 = "HTHTSHTSHTHTSHTHTSHTHTHTSHTSHTHTHTHTHTHTSHTSHTHTSHTSHTSHTSHTHTSHTSHTSHTHTHTHTHTHTSHTSHTHTSHTSHTSHTHTHTSHTSHTSHTSHTSHTSHTSHTHTHTHTHTSHTSHTSHTSHTSHTSHTHTHTHTHTSHTHTSHTHTHTSHTSHTSHTHTSHTSHTHTSHTHTSHTSHTHTSHTHTHTSHTSHTSHTSHTHTHTHTSHTHTHTSHTHTSHTHTHTSHTHTSHTHTSHTXSSWWW";
+
+    let test_inputs = vec![
+        (1234, gates1),
+        (101, gates1),
+        (1, gates1),
+    ];
+
+    for (seed, expected_gates) in test_inputs {
+        let mut gridsynth_config = config_from_theta_epsilon(theta, epsilon, seed);
+        let gates = gridsynth_gates(&mut gridsynth_config);
+        assert_eq!(gates, expected_gates, "Test failed for seed: {}", seed);
+    }
+}
+
+
+//#[ignore]
+#[test]
+fn pi_over_two_test() {
+    let pi = std::f64::consts::PI;
+    let theta = pi / 2.0;
+
+    let epsilons = vec![1e-2, 1e-3, 1e-10];
+
+    for epsilon in epsilons {
+        let seeds = 100..300;
+        for seed in seeds {
+            let mut gridsynth_config = config_from_theta_epsilon(theta, epsilon, seed);
+            let gates = gridsynth_gates(&mut gridsynth_config);
+            let expected_gates = "SWWWWWWW";
+            assert_eq!(gates, expected_gates);
+        }
+    }
 }
